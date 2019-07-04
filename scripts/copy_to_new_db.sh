499039b765a67a6064d0bcab065ac9858617356c
Ben Eppel <ben@beneppelsoftware.com>
2019-07-04 17:18:02 +0100

    DRT-5567 Add restore line to backup script

--- scripts/copy_to_new_db.sh
+++ scripts/copy_to_new_db.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 set -u
 
+echo Dumping from $SOURCE_PGHOST
 env PGHOST=$SOURCE_PGHOST PGPASSWORD=$SOURCE_PGPASSWORD PGUSER=$SOURCE_PGUSER pg_dump -c > /var/data/${PGDATABASE}_dump.sql
+
+echo Restoring to $PGHOST
+psql < /var/data/${PGDATABASE}_dump.sql
+
+rm /var/data/${PGDATABASE}_dump.sql
